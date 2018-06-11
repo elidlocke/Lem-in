cf504fe14c41d087acfd3ae4be10707ada3d6657
Jacob Polloreno <makaveliix16@gmail.com>
2018-06-11 16:12:32 -0700

    add parsing for python visualizer

---
+++ visualizer/read_input.py
@@ -0,0 +1,80 @@
+import argparse
+import os
+import re
+import sys
+
+from pprint import pprint
+
+
+def parse_map(cfg, map_path):
+    patterns = {
+        'tunnel': re.compile(r'^([\d\w]+)-([\d\w]+)\n$'),
+        'nodes': re.compile(r'^([\d\w]+) (\d+) (\d+)\n$'),
+        'start': re.compile(r'^##start\n$'),
+        'end': re.compile(r'^##end\n$'),
+        'ants': re.compile(r'^(\d+)\n$')
+    }
+
+    with open(map_path, 'r') as f:
+        for line in f:
+            for pattern, case in patterns.items():
+                for match in case.findall(line):
+                    if pattern in ['start', 'end']:
+                        match = patterns['nodes'].search(next(f))
+                        cfg[pattern] = match.groups(1)
+                        cfg['nodes'].append(match.groups())
+                    elif pattern is 'ants':
+                        cfg[pattern] = match
+                    else:
+                        cfg[pattern].append(match)
+
+    return cfg
+
+
+def parse_sol(cfg, sol_path):
+    re_turns = re.compile(r'L([\d\w]+)-([\d\w]+)')
+
+    if not sol_path:
+        iterator = sys.stdin.read().splitlines()
+    else:
+        with open(sol_path, 'r') as f:
+            iterator = f.readlines()
+
+    for line in iterator:
+        entry = re_turns.findall(line)
+        if entry:
+            cfg['num_turns'] += 1
+            cfg['turns'].append(entry)
+
+    if not cfg['num_turns']:
+        return None
+    return cfg
+
+
+def parse(map_path, sol_path=None):
+    cfg = {
+        'tunnel': [],
+        'nodes': [],
+        'turns': [],
+        'start': None,
+        'end': None,
+        'ants': None,
+        'num_turns': 0,
+    }
+    return parse_sol(parse_map(cfg, map_path), sol_path)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("map", help="Path to map file", type=str)
+    parser.add_argument("-s", "--solution",
+                        help="File path to solution", type=str)
+    args = parser.parse_args()
+
+    if not os.path.exists(args.map):
+        raise IOError("Map file not found")
+
+    if args.solution and not os.path.exists(args.solution):
+        raise IOError("Solution file not found")
+
+    pprint(parse(args.map, args.solution))
